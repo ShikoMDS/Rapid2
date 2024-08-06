df404597a7b760bca533a4b7535beaeebd138d47
ReubzNZ <reuben.foord1@gmail.com>
2024-08-06 17:38:07 +1200

    Added Tutorial button to the Main menu
    Ayoub you can update it but it looks Perfectly fine the way it is There should be no problems.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,6 +15,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("CutScene");
     }
 
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+
     public void Controls()
     {
         SceneManager.LoadScene("Controls");
