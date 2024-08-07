4f77a2f572bb68e6516f209b02f586329d25ddb2
Christopher Van Houdt MDS <christopher.houdt@mds.ac.nz>
2024-08-07 13:41:29 +1200

    Added prompts to steal

--- Assets/Scripts/Objects/BreakableController.cs
+++ Assets/Scripts/Objects/BreakableController.cs
@@ -8,20 +8,34 @@ public class BreakableController : MonoBehaviour
     public int iRewardedPoints = 1;
     bool bIsBroken = false;
 
- 
+    public GameObject m_text;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (collision.tag == "Player" && bIsBroken == false)
+        {
+            m_text.SetActive(true);
+        }
+
         if (collision.tag == "Player" && Input.GetKey(KeyCode.E) && (bIsBroken == false))
         {
             bIsBroken = true;
             pPlayer.iPoints += iRewardedPoints;
+            m_text.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            m_text.SetActive(false);
         }
     }
 
     private void Start()
     {
         pPlayer = FindAnyObjectByType<PlayerScript>();
+        m_text.SetActive(false);
     }
-
-   
 }
--- Assets/Scripts/Objects/CollectableController.cs
+++ Assets/Scripts/Objects/CollectableController.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class CollectableController : MonoBehaviour
 {
     PlayerScript pPlayer;
-    public int iRewardedPoints = 1; 
+    public int iRewardedPoints = 1;
+    public GameObject m_text;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (collision.tag == "Player")
+        {
+            m_text.SetActive(true);
+        }
+
         if (collision.tag == "Player" && Input.GetKey(KeyCode.E))
         {
             Destroy(gameObject);
             pPlayer.iPoints += iRewardedPoints;
         }
     }
-    
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            m_text.SetActive(false);
+        }
+    }
+
     private void Start()
     {
         pPlayer = FindAnyObjectByType<PlayerScript>();
+        m_text.SetActive(false);
     }
 }
